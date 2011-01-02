d3fe96a8afef5cae60682e3c76c7802402ba0cdb
Dave Brosius <dbrosius@mebigfatguy.com>
2011-01-02 13:17:41 -0500

    stub in Operand Stack/Operand

---
+++ src/com/mebigfatguy/junitflood/jvm/Operand.java
@@ -0,0 +1,41 @@
+/*
+ * junitflood - An automatic junit test generator
+ * Copyright 2011 MeBigFatGuy.com
+ * Copyright 2011 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.junitflood.jvm;
+
+public class Operand {
+	private final int register;
+	private final String field;
+
+	public Operand(int reg) {
+		register = reg;
+		field = null;
+	}
+
+	public Operand(String fld) {
+		register = -1;
+		field = fld;
+	}
+
+	public int getRegister() {
+		return register;
+	}
+
+	public String getField() {
+		return field;
+	}
+}
